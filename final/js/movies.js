6a5e71e77eacf014fa96088a9fbc231a8899895f
Lewis-Kate <35283629+Lewis-Kate@users.noreply.github.com>
2020-07-14 22:19:40 -0400

    final

---
+++ final/js/movies.js
@@ -0,0 +1,40 @@
+function findMovie() {
+	// Get the value from the search box
+	let searchTitle = $("#title").val();
+	console.log("Searching for: " + searchTitle);
+
+	// Set up the parameters to send to the API
+	let params = {s: searchTitle, apikey:"c214de2e"};
+
+	// Use jQuery to make the get request
+	$.get("https://www.omdbapi.com/", params, function(data, status){
+		// For debugging purposes, make a note that we're back
+		console.log("Results from API:")
+		console.log(status);
+    	console.log(data);
+
+    	updateResults(data)
+	});
+}
+
+function updateResults(data) {
+
+	if (data.Search && data.Search.length > 0) {
+		var resultList = $("#dataResults");
+		resultList.empty();
+
+		for (let i = 0; i < data.Search.length; i++) {
+			let title = data.Search[i].Title;
+			let poster = data.Search[i].Poster;
+            let year = data.Search[i].Year;
+            let imdbID = data.Search[i].imdbID;
+			resultList.append("<div class='title imgContainer'><a href='http://www.imdb.com/title/" + imdbID
+ + "' target='_blank'/'><img class='poster' src='" + poster + "' alt='" + title + year + "'><div class='transparentShield'><p class='titleText'>" + title +' '+ year + "</p></div></a></div>");
+		} 
+	} else {
+			let resultList = $("#dataResults");
+			resultList.empty();
+			resultList.append("<p class='title warning'>>Please try again</p>");
+		}
+
+}
